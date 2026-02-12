b2b1ec77fb62a27ea9fe09b32393230d317f14e2
Alex-935 <alex.potter3@hotmail.co.uk>
2026-02-12 21:38:18 +0000

    BlackJack CommandLine

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class App {
+
+    public static void main(String[] args) {
+
+        BlackJackDeck deck = new BlackJackDeck();
+        Scanner scanner = new Scanner(System.in);
+        BlackJack blackJack = new BlackJack(deck, scanner);
+    }
 }
--- src/main/java/BlackJackDeck.java
+++ src/main/java/BlackJackDeck.java
@@ -1,18 +1,27 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class Deck {
+public class BlackJackDeck {
 
     private List<BlackJackCard> deck = new ArrayList<>(0);
     private String[] suits = {"clubs, diamonds, hearts, spades"};
     private String[] faces = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
 
-    public Deck() {
+    public BlackJackDeck() {
 
         for (int suit = 0; suit < 4; suit++) {
-            for (int face = 0; face < 13; face++) {
+            for (int face = 1; face < 14; face++) {
                 deck.add(new BlackJackCard(suit, face));
             }
         }
+    }
+
+    public BlackJackCard getNextCard(int currIndex) {
+        return this.deck.get(currIndex);
+    }
+
+    public void shuffle() {
+        Collections.shuffle(this.deck);
     }
 }
